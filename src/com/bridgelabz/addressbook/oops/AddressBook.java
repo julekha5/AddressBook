cb75bce2b9f7d2e5e4dd2d7e56cc97261be1d045
Julekha5 <mulani.julekha93@gmail.com>
2021-12-16 16:03:44 +0530

    Added:Welcome message to master branch

---
+++ src/com/bridgelabz/addressbook/oops/AddressBook.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.addressbook.oops;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to AddressBook Program");
+
+	}
+}
